d559cee31bfbaeb69bc1241e8281377b6e9ed88d
Elena Poelman <elena.poelman@redpencil.io>
2022-08-05 16:53:59 +0200

    update env variables

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
-export const LDES_ENDPOINT_VIEW =
-  process.env.LDES_ENDPOINT_VIEW || 'http://localhost:3000/test/1';
-export const CRON_PATTERN = process.env.CRON_PATTERN || '0 * * * * *';
+export const LDES_ENDPOINT_VIEW = process.env.LDES_ENDPOINT_VIEW;
+export const CRON_PATTERN = process.env.CRON_PATTERN || '* 0 * * * *';
 export const DATA_FOLDER = './data';
 export const JSON_ENDPOINT =
+  process.env.JSON_ENDPOINT ||
   'https://ipdc.ipdc.tni-vlaanderen.be/api/abb/instanties';
