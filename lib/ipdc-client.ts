55a15bdaefa60cbf9b5e253903b535c883891105
Elena Poelman <elena.poelman@redpencil.io>
2022-08-08 09:36:21 +0200

    replace flatten by expand

--- lib/ipdc-client.ts
+++ lib/ipdc-client.ts
@@ -4,7 +4,7 @@ import { JSON_ENDPOINT } from '../config';
 export async function processMember(member: Member) {
   try {
     const fromRdf = await jsonld.fromRDF(member.quads);
-    const doc = await jsonld.flatten(fromRdf);
+    const doc = await jsonld.expand(fromRdf);
     const options: RequestInit = {
       method: 'PUT',
       headers: { 'Content-type': 'application/ld+json' },
@@ -12,6 +12,6 @@ export async function processMember(member: Member) {
     };
     await fetch(JSON_ENDPOINT, options);
   } catch (e) {
-    console.log(`Processing member failed: ${e}`);
+    console.error(`Processing member failed: ${e}`);
   }
 }
